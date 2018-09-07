6e072bd153b480bc9ea16b7908affcddb67918da
Mike <mmarceca@aardvarkbrigade.com>
2018-09-07 11:52:51 -0400

    wip

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -1,5 +1,5 @@
 import { connect } from 'react-redux';
-import React from ('react');
-import store from ('store')
-import * as types from ('./constants/actions');
+import React from 'react';
+import store from 'store';
+import * as types from './constants/actions';
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
-import React from ('react');
-import { render } from ('react-dom');
-import App from ('./components/app');
-import store from ('./components/store');
-import { Provider } from ('react-redux');
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/app';
+import store from './components/store';
+import { Provider } from 'react-redux';
 import styles from './wherever/styles/is';
 
 render(
