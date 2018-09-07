f952884741030cba97e187fb801e192046e8e45c
Mike <mmarceca@aardvarkbrigade.com>
2018-09-07 11:55:47 -0400

    added react redux

--- server2.js
+++ server2.js
@@ -6,31 +6,31 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
- 
- var filePath = '.' + req.url;
- if (filePath == './') {
-     filePath = './index.html';
- }
 
- var extname = String(path.extname(filePath)).toLowerCase();
- var mimeTypes = {
-     '.html': 'text/html',
-     '.js': 'text/javascript',
-     '.css': 'text/css',
-     '.json': 'application/json',
-     '.png': 'image/png',
-     '.jpg': 'image/jpg',
-     '.gif': 'image/gif',
-     '.wav': 'audio/wav',
-     '.mp4': 'video/mp4',
-     '.woff': 'application/font-woff',
-     '.ttf': 'application/font-ttf',
-     '.eot': 'application/vnd.ms-fontobject',
-     '.otf': 'application/font-otf',
-     '.svg': 'application/image/svg+xml'
- };
+var filePath = '.' + req.url;
+if (filePath == './') {
+    filePath = './index.html';
+}
 
- var contentType = mimeTypes[extname] || 'application/octet-stream';
+var extname = String(path.extname(filePath)).toLowerCase();
+var mimeTypes = {
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.css': 'text/css',
+    '.json': 'application/json',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.gif': 'image/gif',
+    '.wav': 'audio/wav',
+    '.mp4': 'video/mp4',
+    '.woff': 'application/font-woff',
+    '.ttf': 'application/font-ttf',
+    '.eot': 'application/vnd.ms-fontobject',
+    '.otf': 'application/font-otf',
+    '.svg': 'application/image/svg+xml'
+};
+
+var contentType = mimeTypes[extname] || 'application/octet-stream';
 
 fs.readFile(filePath, function(error, content) {
         if (error) {
@@ -52,8 +52,8 @@ fs.readFile(filePath, function(error, content) {
         }
     });
 
-  });
+});
 
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
+server.listen(port, hostname, () => {
+console.log(`Server running at http://${hostname}:${port}/`);
+});
