a868f8baf88a4317ebcdc877b52bbc84b0194e80
james-owen <james@jamesowen.io>
2018-09-07 11:02:37 -0400

    Expanded server boilerplate

--- server.js
+++ server.js
@@ -9,7 +9,18 @@ http.createServer((request, response) => {
     body.push(chunk);
   }).on('end', () => {
     body = Buffer.concat(body).toString();
-    // At this point, we have the headers, method, url and body, and can now
-    // do whatever we need to in order to respond to this request.
+
+    response.on('error', (err) => {
+      console.error(err);
+    });
+
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'application/json');
+
+    const responseBody = { headers, method, url, body };
+
+    response.write(JSON.stringify(responseBody));
+    response.end();
+
   });
-}).listen(3002); // Activates this server, listening on port 8080.
+}).listen(3000);
