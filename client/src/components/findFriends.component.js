f81c99fe46a29faae3075e67789d5ab0e0543b87
Mike <mmarceca@aardvarkbrigade.com>
2018-09-07 20:44:57 -0400

    removed consoles and changed up the style a bit for find friends page

--- client/src/components/findFriends.component.js
+++ client/src/components/findFriends.component.js
@@ -8,13 +8,10 @@ const mapStateToProps = store => ({
 
 
 const FindFriends = (props) => {
-  console.log(props.findFriends.users)
   let friendsArr = [];
   for(let i = 0; i < props.findFriends.users.length; i++) {
-    console.log(props.findFriends.users[i])
     friendsArr.push(<div className="friend-box" key={"friend" + i}><div className="inner-box"><span className="friend-box__user">User:     </span>{props.findFriends.users[i].user}</div><div className="inner-box"><span className="friend-box__cuisine"> Cuisine:     </span>{props.findFriends.users[i].cuisine}</div></div>)
   }
-  console.log(friendsArr)
   return (
     <div className="find-friends-container">
       <div className="main-header">Find Friends</div>
